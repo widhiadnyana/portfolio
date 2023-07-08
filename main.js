273ec8c63ccd9a3bc0c9b29e568f04350af3f4a0
widhi <adnyanaw60@gmail.com>
2023-07-08 16:28:41 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = navClicked;
+
+function navClicked(params) {
+  const mobileNav = document.getElementById("mobile-id");
+  if (mobileNav.style.display === "flex") {
+    mobileNav.style.display = "none";
+  } else {
+    mobileNav.style.display = "flex";
+  }
+  console.log(mobileNav.style.display);
+}
